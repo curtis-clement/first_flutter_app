e7de12bda7e85d64f82316effad60dcd4a4eaafe
Curtis Clement <curtis.clement@wonderkind.com>
2024-07-30 01:37:01 +0200

    add background colour and gradient

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello, World!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.cyan, Color.fromARGB(255, 233, 37, 102)],
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello, World!'),
+          ),
         ),
       ),
     ),
