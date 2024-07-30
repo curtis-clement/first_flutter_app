c19e5d164a4d28370916efc43b7900697c3f0c2f
Curtis Clement <curtis.clement@wonderkind.com>
2024-07-30 14:09:40 +0200

    refactor into first class / custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,30 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Colors.cyan, Color.fromARGB(255, 233, 37, 102)],
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello, World!'),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Colors.cyan,
+            Color.fromARGB(255, 233, 37, 102),
+          ],
+        ),
+      ),
+      child: const Center(
+        child: Text('Hello, World!',
+            style: TextStyle(color: Colors.white, fontSize: 48,)),
+      ),
+    );
+  }
 }
